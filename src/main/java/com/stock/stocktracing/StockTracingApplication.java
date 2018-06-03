70a3eea1ac2ca85b79a0975cb35d072449a2a5cf
ykyo.jung <ykyo.jung@coupang.com>
2018-06-03 11:23:53 +0900

    project create

---
+++ src/main/java/com/stock/stocktracing/StockTracingApplication.java
@@ -0,0 +1,12 @@
+package com.stock.stocktracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTracingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTracingApplication.class, args);
+	}
+}
