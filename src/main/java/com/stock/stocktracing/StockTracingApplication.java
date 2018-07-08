b23d969bb9dc77cdc0b7991d7934ac5d4772c633
ykyo.jung <ykyo.jung@coupang.com>
2018-07-08 15:25:08 +0900

    aws CodeDeploy 연동

--- src/main/java/com/stock/stocktracing/StockTracingApplication.java
+++ src/main/java/com/stock/stocktracing/StockTracingApplication.java
@@ -1,12 +1,23 @@
 package com.stock.stocktracing;
 
-import org.springframework.boot.SpringApplication;
+import javafx.application.Application;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
-public class StockTracingApplication {
+public class StockTracingApplication{
+
+	private static final String APPLICATION_LOCATIONS = "spring.config.location="
+		+ "classpath:application.yml,"
+		+ "/app/config/stock-tracing/real-application.yml";
+
 
 	public static void main(String[] args) {
-		SpringApplication.run(StockTracingApplication.class, args);
+//		SpringApplication.run(StockTracingApplication.class, args);
+		new SpringApplicationBuilder(StockTracingApplication.class)
+			.properties(APPLICATION_LOCATIONS)
+			.run(args);
 	}
 }
--- src/main/java/com/stock/stocktracing/web/WebRestController.java
+++ src/main/java/com/stock/stocktracing/web/WebRestController.java
@@ -6,15 +6,19 @@ import com.stock.stocktracing.domain.service.MemberRecommendStockService;
 import com.stock.stocktracing.domain.service.PostsService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+
 @AllArgsConstructor
 @RestController
 public class WebRestController {
 
+	private Environment env;
 
 	@Autowired
 	private PostsService service;
@@ -35,5 +39,12 @@ public class WebRestController {
 	@PostMapping("/stock/Register")
 	public void saveMemberRecommendStock(@RequestBody MemberRecommendStockDto recommendStockDto) {
 		memberRecommendStockService.save(recommendStockDto);
+	}
+
+	@GetMapping("/profile")
+	public String getProfile () {
+		return Arrays.stream(env.getActiveProfiles())
+			.findFirst()
+			.orElse("");
 	}
 }
